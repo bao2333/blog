2336530e3b4e171c7f65005cc1ade398f17d4773
qin <15936669602@163.com>
2020-04-17 10:07:17 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,69 +1,32 @@
 module.exports = {
-    base: '/',
-    title: '前端总结',
-    description: '前端总结',
-    head: [
-        ['link', {
-            rel: 'icon',
-            href: '/logo.png'
-        }]
+  title: '一个前端小青年的学习笔记',
+  description: 'good good study, day day up',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+  ],
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'vue源码解析', link: '/vue源码解析/' },
+      { text: 'javaScript', link: '/javaScript/' },
+      { text: '数据结构与算法', link: '/dataStructureAndAlgorithm/'},
     ],
-    host: '192.168.2.124',
-    post: 8080,
-    dest: '_site',
-    cache: true,
-    themeConfig: {
-        navbar: true,
-        locales: {
-            '/': {
-                nav: [{
-                    text: 'vue源码解析',
-                    link: '/vue/'
-                }, {
-                    text: 'javaScript',
-                    link: '/javaScript/'
-                }, {
-                    text: '数据结构与算法',
-                    link: '/data/'
-                }],
-                sidebar: {
-                    '/vue/': [
-                        ['', '源码分析',]
-                    ],
-                    '/javaScript/': [
-                        ['', '简介'],
-                    ]
-                }
-            },
-        }
-    },
-    markdown: {
-        lineNumbers: true,
-        anchor: {
-            permalink: true,
-            permalinkBefore: true,
-            permalinkSymbol: '#'
-        },
-        externalLinks: {
-            target: '_blank',
-            rel: 'noopener noreferrer'
-        },
-        toc: {
-            includeLevel: [2, 3]
-        },
-        plugins: []
-    },
-    editLinkText: '在 GitHub 上编辑此页',
-    postcss: {
-        plugins: [require('autoprefixer')]
-    },
-    stylus: {
-        preferPathResolver: 'webpack'
-    },
-    scss: {},
-    sass: {
-        indentedSyntax: true
-    },
-    less: {},
-    evergreen: false,
+    sidebar: {
+      '/vue源码解析/': [
+        '',
+        'CREATEELEMENT'
+      ],
+      '/javaScript/': [
+        '',
+        'TALKABOUT',
+        'PROTOTYPE',
+        '模块化'
+      ],
+      '/dataStructureAndAlgorithm/': [
+        '',
+        'DATASTRUCT',
+
+      ]
+    }
+  },
 }
